e9230fabae352a0c2dc5249318e5bcc927addc42
marcomarinho <marcomarinho12@gmail.com>
2022-05-01 11:09:22 +0100

    Revert "hwc2: add support for IDisplayConfig 2.0"
    This reverts commit 0d003b1d299ebae53aa41f2b6d6d1f1e47abfab9.

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -1512,7 +1512,7 @@ android::status_t HWCSession::notifyCallback(uint32_t command, const android::Pa
         DLOGE("QService command = %d: input_parcel needed.", command);
         break;
       }
-      status = SetIdleTimeout(UINT32(input_parcel->readInt32()));
+      status = setIdleTimeout(UINT32(input_parcel->readInt32()));
       break;
 
     case qService::IQService::SET_FRAME_DUMP_CONFIG:
@@ -1547,7 +1547,7 @@ android::status_t HWCSession::notifyCallback(uint32_t command, const android::Pa
         int disp_id = INT(input_parcel->readInt32());
         HWCDisplay::DisplayStatus disp_status =
               static_cast<HWCDisplay::DisplayStatus>(input_parcel->readInt32());
-        status = SetDisplayStatus(disp_id, disp_status);
+        status = SetSecondaryDisplayStatus(disp_id, disp_status);
         output_parcel->writeInt32(status);
       }
       break;
@@ -1570,7 +1570,7 @@ android::status_t HWCSession::notifyCallback(uint32_t command, const android::Pa
           break;
         }
         int32_t input = input_parcel->readInt32();
-        status = ToggleScreenUpdate(input == 1);
+        status = toggleScreenUpdate(input == 1);
         output_parcel->writeInt32(status);
       }
       break;
@@ -1696,7 +1696,7 @@ android::status_t HWCSession::notifyCallback(uint32_t command, const android::Pa
           break;
         }
         uint32_t camera_status = UINT32(input_parcel->readInt32());
-        status = SetCameraLaunchStatus(camera_status);
+        status = setCameraLaunchStatus(camera_status);
       }
       break;
 
@@ -3575,7 +3575,7 @@ android::status_t HWCSession::SetIdlePC(const android::Parcel *input_parcel) {
   auto enable = input_parcel->readInt32();
   auto synchronous = input_parcel->readInt32();
 
-  return static_cast<android::status_t>(ControlIdlePowerCollapse(enable, synchronous));
+  return static_cast<android::status_t>(controlIdlePowerCollapse(enable, synchronous));
 }
 
 hwc2_display_t HWCSession::GetActiveBuiltinDisplay() {
